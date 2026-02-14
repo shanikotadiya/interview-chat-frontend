79e4c591919b9c9308e103fda2c965abc3531cf3
Shani Kotadiya <shanikotadiya@gmail.com>
2026-02-14 12:58:14 +0530

    Setup global SCSS styling and layout foundation

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.scss";
+import "@/styles/globals.scss";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,7 +19,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} dashboardLayout`}>
         {children}
       </body>
     </html>
